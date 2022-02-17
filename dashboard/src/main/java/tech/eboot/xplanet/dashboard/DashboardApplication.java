ec3a8ad332f41941bbfd32abfb6710af3e969cd0
TangThree <fandareliu@gmail.com>
2022-02-17 18:39:10 +0800

    1

---
+++ dashboard/src/main/java/tech/eboot/xplanet/dashboard/DashboardApplication.java
@@ -0,0 +1,9 @@
+package tech.eboot.xplanet.dashboard;
+
+/**
+ * @author TangThree
+ * Created on 2022/2/17 14:14
+ **/
+public class DashboardApplication
+{
+}
---
+++ storage/src/main/java/tech/eboot/xplanet/storage/MessageStorageService.java
@@ -0,0 +1,9 @@
+package tech.eboot.xplanet.storage;
+
+/**
+ * @author TangThree
+ * Created on 2022/2/17 14:09
+ **/
+public interface MessageStorageService
+{
+}
