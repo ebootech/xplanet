c406daf55ee35a95b0f9cb4cff307aeca926a955
TangThree <fandareliu@gmail.com>
2022-02-17 18:44:22 +0800

    1

--- dashboard/src/main/java/tech/eboot/xplanet/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/tech/eboot/xplanet/dashboard/DashboardApplication.java
@@ -1,9 +1,18 @@
 package tech.eboot.xplanet.dashboard;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author TangThree
  * Created on 2022/2/17 14:14
  **/
+
+@SpringBootApplication
 public class DashboardApplication
 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
 }
